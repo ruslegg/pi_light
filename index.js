b0c50a92667e132c4bff6433a5e4c3a3ca86dd7d
ruslegg <negrei.ruslan@gmail.com>
2020-05-27 04:29:44 +0200

    added schedule, https, changed logic how light operates

--- index.js
+++ index.js
@@ -1,110 +1,184 @@
-var express = require("express");
-var app = express();
-const PORT = 3000;
-var lightStatus = "off";
-var luminositySensor = "on";
-var luminosityLevel = 0;
-var threshold = 0;
-var init = true;
+  var express = require("express");
+  var app = express();
+  const PORT = 3000;
+  var lightStatus = "sensor";
+  var luminositySensor = "on";
+  var luminosityLevel = 0;
+  var threshold = 0;
+  var init = true;
+  var https = require('https'); 
+  var http = require('http'); 
+  var fs = require('fs');
+  var CronJob = require('cron').CronJob;
+  var scheduledJobFrom = '';
+  var scheduledJobTo = '';
+  var isLightOn = false;
+  var options = {
+    key: fs.readFileSync(__dirname + '/certs/privkey.pem'),
+    cert: fs.readFileSync(__dirname + '/certs/fullchain.pem'),
 
+  }
+  var schedule = {};
+  var five = require("johnny-five"),
+      board = new five.Board();
+  
+  board.on("ready", function() {
+    console.log('here again');  
+  
+    const relay = new five.Relay(2)
+  
+    let lightSensor = new five.Sensor({
+      pin:  "A0",
+      freq: 250,
+      threshold: 5
+    })
+    lightSensor.on("change", function () {
+      luminosityLevel = this.scaleTo(0,100);
+  
+      if (init) {
+        threshold = luminosityLevel;
+        init = false;
+      }
+      if (lightStatus === 'on' || lightStatus === 'sensor' && luminosityLevel <= threshold) {
+        relay.on();
+        isLightOn = true;
+      }
+      else if (lightStatus === 'off' || lightStatus === 'sensor' && luminosityLevel > threshold) {
+        relay.off();
+        isLightOn = false;
+      }
+      console.log(`Light is now ${isLightOn ? 'on' : 'off'} Threshold is now: ${threshold}, Luminosity level is now ${luminosityLevel}`);
 
-var five = require("johnny-five"),
-    board = new five.Board();
+    })
+    // Create an Led on pin 13
+    this.repl.inject({
+      relay: relay
+    });
 
-board.on("ready", function() {
-  console.log('here again');  
-
-  const relay = new five.Relay(2)
-
-  let lightSensor = new five.Sensor({
-    pin:  "A0",
-    freq: 250,
-    threshold: 5
+  app.use((req, res, next) => {
+    if (req.secure) {
+      next();
+    } else {
+      res.redirect('https://' + req.headers.host + req.url);
+    }
   })
-  lightSensor.on("change", function () {
-    luminosityLevel = this.scaleTo(0,100);
-    console.log(`Light is now ${lightStatus}, Luminosity sensor is now ${luminositySensor}, Threshold is now: ${threshold}, Luminosity level is now ${luminosityLevel}`);
-
-    if (init) {
-      threshold = luminosityLevel;
-      init = false;
-    }
-    if (lightStatus === 'on') {
-      return relay.on();
-    }
-    if (luminositySensor === 'on' && luminosityLevel <= threshold) {
-      relay.on();
-    }
-    else if (luminositySensor === 'off' || luminosityLevel > threshold) {
-      relay.off();
-    }
-  })
-  // Create an Led on pin 13
-  this.repl.inject({
-    relay: relay
+    
+  app.get("/", (req, res) => {
+    console.log("received request");
+    res.sendStatus(404);
   });
   
-  var led = new five.Led(13);
+  app.get("/manualLight/:mode", (req, res) => {
+    const mode = req.params.mode;
+    if (mode !== "on" && mode !== "off") {
+      res.sendStatus(403);
+    }
+    lightStatus = mode;
+    if (lightStatus === 'on') {
+      relay.on();
+    } else {
+      relay.off();
+    }
+    console.log(`light: ${lightStatus}`);
+    res.send({
+      title: `Success`,
+      message: `Light is ${lightStatus}`,
+    });
+  });
 
-  // Strobe the pin on/off, defaults to 100ms phases
-  led.strobe(1000);
-  // respond with "hello world" when a GET request is made to the homepage
-app.get("/", (req, res) => {
-  console.log("received request");
-  res.sendStatus(404);
-});
+  app.get("/sensor", (req, res) => {
+    lightStatus = 'sensor';
+    if (luminosityLevel <= threshold) {
+      relay.on();
+      isLightOn = true;
+    } else {
+      relay.off();
+      isLightOn = false;
+    }
+    res.send({
+      title: `Success`,
+      message: `Light is sensor-based`,
+    });
+  });
+  
+  app.get("/switch", (req, res) => {
+    var message = '';
+    if (lightStatus === 'on') {
+      message = 'on and manually triggered'
+    } else if (lightStatus === 'off') {
+      message = 'off';
+    } else if (lightStatus === 'sensor') {
+      message = 'on and sensor-based';
+    }
+    res.send({
+      message: `Light is ${message}`,
+    });
+  });
+  
+  app.get("/luminositySensor", (req, res) => {
+    var message = "";
+    if (lightStatus === "sensor") {
+      message =
+        "on and luminosity level is at " + luminosityLevel + " percentage";
+    } else {
+      message = 'off';
+    }
+    res.send({
+      message: `Light based on luminosity sensor is now ${message}`,
+    });
+  });
 
-app.get("/switch/:mode", (req, res) => {
-  const mode = req.params.mode;
-  if (mode !== "on" && mode !== "off") {
-    res.sendStatus(403);
-  }
-  lightStatus = mode;
-  if (lightStatus === 'on') {
+  app.get("/threshold", (req, res) => {
+    threshold = parseInt(luminosityLevel);
     relay.on();
-  } else {
-    relay.off();
-  }
-  console.log(`light: ${lightStatus}`);
-  res.send({
-    title: `Success`,
-    message: `Light is ${lightStatus}`,
+    isLightOn = true;
+    res.send({
+      message: `Threshold has been changed to the current luminosity level: ${luminosityLevel}`,
+    });
   });
-});
 
-app.get("/switch", (req, res) => {
-  res.send({
-    message: `Light is ${lightStatus}`,
+  app.get("/schedule/:offFrom/:offTo", (req, res) => {
+    const {offFrom, offTo} = req.params;
+    if (scheduledJobFrom != '') {
+      scheduledJobFrom.stop();
+    }
+    if (scheduledJobTo != '') {
+      scheduledJobTo.stop();
+    }
+    let dividedString = offFrom.split(':');
+    scheduledJobFrom = new CronJob(dividedString[1] + ' ' + dividedString[0] + ' * * *', function() {
+      relay.off();
+      lightStatus = 'off';
+      isLightOn = false;
+      console.log('ranFrom')
+    }, null, true, 'Europe/Copenhagen');
+    console.log(dividedString);
+    dividedString = offTo.split(':');
+    scheduledJobTo = new CronJob(dividedString[1] + ' ' + dividedString[0] + ' * * *', function(){
+      relay.off();
+      lightStatus = 'sensor';
+      if (luminosityLevel <= threshold) {
+        relay.on();
+        isLightOn = true;
+      }
+      console.log('ran');
+    }, null, true, 'Europe/Copenhagen');
+    console.log(dividedString);
+
+    res.send({
+      message: `Schedule set to switch off light from ${offFrom} to ${offTo}`
+    })
   });
-});
-
-app.get("/luminositySensor", (req, res) => {
-  var message = "";
-  if (luminositySensor === "on") {
-    message =
-      " and luminosity level is at " + luminosityLevel + " percentage";
-  }
-  res.send({
-    message: `Light based on luminosity sensor is now ${luminositySensor}${message}`,
-  });
-});
-
-app.get("/luminositySensor/:mode", (req, res) => {
-  const mode = req.params.mode;
-  if (mode !== "on" && mode !== "off") {
-    res.sendStatus(403);
-  }
-  luminositySensor = mode;
-  res.send({
-    title: `Success`,
-    message: `Light based on luminosity sensor is set to ${luminositySensor}`,
-  });
-});
-
-app.listen(PORT, () =>
+  
+  
+  var httpserver = http.createServer(options,app);
+  var httpsserver = https.createServer(options, app);
+  httpserver.listen(80, () =>
+    console.log('http server on')
+  );
+  httpsserver.listen(443, () => {
+  })
   console.log(
     `Raspberry PI + Arduino server light controller listening at: http://localhost:${PORT}`
   )
-);
-  
-});
+  });
